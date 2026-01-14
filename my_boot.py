74a2a07f2527ace12e200d04894fd04d6073b17c
Ghislain d'Andlau <gdandlau@gk-software.com>
2026-01-14 12:33:38 +0100

    second commit

--- my_boot.py
+++ my_boot.py
@@ -1,1 +1,39 @@
-from selenium import *
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+# So window is always open
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+# Create profile -- this I am not 100% sure
+user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+
+# Open the tab
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://appbrewery.github.io/gym/")
+
+#user details
+email = "student@test.com"
+password = "password123"
+
+#login
+login_button = driver.find_element(By.ID, "login-button")
+login_button.click()
+
+driver.implicitly_wait(2)
+
+email_input = driver.find_element(By.ID, "email-input")
+email_input.send_keys(email)
+
+password_input = driver.find_element(By.ID, "password-input")
+password_input.send_keys(password)
+
+submit_button = driver.find_element(By.ID, "submit-button")
+submit_button.click()
+
+logged_in = driver.find_element(By.ID, "schedule-page").is_displayed()
+print(logged_in)
+
