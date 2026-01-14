6a1952a197ce88c3a15f0ebd144dc9d6693fbc77
Ghislain d'Andlau <gdandlau@gk-software.com>
2026-01-14 14:42:30 +0100

    third commit

--- my_boot.py
+++ my_boot.py
@@ -1,7 +1,8 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import Select
+from datetime import datetime, timedelta
 
 # So window is always open
 chrome_options = webdriver.ChromeOptions()
@@ -16,8 +17,8 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://appbrewery.github.io/gym/")
 
 #user details
-email = "student@test.com"
-password = "password123"
+email = "ghislain.dandlau@gmail.com"
+password = "ghislain.dandlau@gmail.com"
 
 #login
 login_button = driver.find_element(By.ID, "login-button")
@@ -36,4 +37,29 @@ submit_button.click()
 
 logged_in = driver.find_element(By.ID, "schedule-page").is_displayed()
 print(logged_in)
+
+# find element
+dropdown = driver.find_element(By.ID, "type-filter")
+drop = Select(dropdown)
+class_wanted = "Spin"
+drop.select_by_visible_text(class_wanted)
+
+# find the next tuesday
+today_date = datetime.now()
+weekday_idx = 1
+
+days_delta = weekday_idx - today_date.weekday()
+if days_delta <= 0:
+    days_delta += 7
+
+res = today_date + timedelta(days_delta)
+
+print(res.date())
+
+next_course_button = driver.find_element(By.ID, f"book-button-{class_wanted.lower()}-{res.date()}-1800")
+print(next_course_button.is_displayed())
+next_course_button.click()
+
+#
+
 
